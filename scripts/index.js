79a6bb97e4814f2c6aa039d7acd238b6c2e4082f
trobins14 <tristan.robins14@gmail.com>
2024-04-17 08:52:17 -0400

    Completed and styled store page

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+// const themeToggleBtn = document.getElementById('theme-toggle-btn');
+
+
+// themeToggleBtn.addEventListener('click', () => {
+
+//   document.body.classList.toggle('dark');
+
+// });
+
+// document.getElementById('btnSwitch').addEventListener('click',()=>{
+//     if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+//         document.documentElement.setAttribute('data-bs-theme','light')
+//     }
+//     else {
+//         document.documentElement.setAttribute('data-bs-theme','dark')
+//     }
+// })
+
+
